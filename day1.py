e1e5aee3552be7c5d153d912ca8b359a6e5ca98e
ZURSONA777 <149936889+ZURSONA7771@users.noreply.github.com>
2024-02-23 13:02:16 +0400

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,302 @@
+from turtle import*
+
+#we want to paint house
+
+#step 1:
+penup()
+goto(-450, -70)
+pendown()
+
+speed(0)
+width(7)
+color('green')
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+begin_fill()
+
+#drawing a door
+
+begin_fill()
+forward(70)
+color('black')
+left(90)
+forward(120)    #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(-250, 130)
+pendown()
+
+color('red')
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#and
+
+#add window
+
+width(3)
+penup()
+goto(-250, 100)
+pendown()
+
+begin_fill()
+color('blue')
+left(30)
+right(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+end_fill()
+
+penup()
+goto(-450, 60)
+pendown()
+
+begin_fill()
+color('blue')
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+end_fill()
+
+left(90)
+penup()
+goto(-200, -70)
+pendown()
+
+speed(0)
+width(7)
+color('green')
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+begin_fill()
+
+#drawing a door
+
+begin_fill()
+forward(70)
+color('black')
+left(90)
+forward(120)    #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(0, 130)
+pendown()
+
+color('red')
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#and
+
+#add window
+
+width(3)
+penup()
+goto(0, 100)
+pendown()
+
+begin_fill()
+color('blue')
+left(30)
+right(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+end_fill()
+
+penup()
+goto(-200,60)
+pendown()
+
+begin_fill()
+color('blue')
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+end_fill()
+
+#END 2 HOME 
+
+left(90)
+penup()
+goto(40, -70)
+pendown()
+
+speed(0)
+width(7)
+color('green')
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+begin_fill()
+
+#drawing a door
+
+begin_fill()
+forward(70)
+color('black')
+left(90)
+forward(120)    #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(240, 130)
+pendown()
+
+color('red')
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#and
+
+#add window
+
+width(3)
+penup()
+goto(80, 100)
+pendown()
+
+begin_fill()
+color('blue')
+left(30)
+right(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+end_fill()
+
+penup()
+goto(200,60)
+pendown()
+
+begin_fill()
+color('blue')
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+end_fill()
+
+penup()
+goto(-670 , 308)
+pendown()
+
+begin_fill()
+color("yellow")
+circle(60)
+end_fill()
+
+penup()
+goto(10 , 395)
+pendown()
+
+penup()
+goto(-670 , -12)
+pendown()
+
+begin_fill()
+color("brown")
+left(180)
+forward(145)
+right(90)
+forward(45)
+right(90)
+forward(145)
+right(90)
+forward(45)
+end_fill()
+
+penup()
+goto(-660 , 135)
+pendown()
+
+begin_fill()
+right(205)
+color("green")
+circle(65)
+end_fill()
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
