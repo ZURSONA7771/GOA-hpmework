b2f9d529e67cbf37216e92511b3d7c103c523dd4
ZURSONA777 <149936889+ZURSONA7771@users.noreply.github.com>
2024-03-03 17:42:23 +0400

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,8 @@
+name = input(' tell us your name:   ')
+age = int(input(' tell us  your age: '))
+time_travel = int(input('tell us for how much years you want to travel in the future:'))
+text = input('please enter if you want to see how meny years old you will be when you travel in the furture:')
+
+
+print(text)
+print(age + time_travel)
