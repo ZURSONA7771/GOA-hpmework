f75185ddcf816d75891fa68cdc408a3b44313166
ZURSONA777 <149936889+ZURSONA7771@users.noreply.github.com>
2024-03-09 23:43:30 +0400

    Add files via upload

--- lesson2.py
+++ lesson2.py
@@ -1,8 +1,11 @@
 
+print(10 / 2)
+print(100 % 4)
 
+print(20 / 2)
+print(10 // 2)
+print(120 % 2)
 
-num1=int(input('enter number'))
-num2=int(input('enter number'))
-
-print(num1+num2)
-
+print(450 / 2)
+print(500 // 5)
+print(200 % 20)
